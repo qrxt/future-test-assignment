9e27bb6b77d47e8ed35939bf1886b32aa5c12c21
qrxt <qrx357@gmail.com>
2020-12-22 23:18:45 +0300

    начало разметки

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import './app.css';
+
+import styles from './app.module.css';
 
 const App = () => (
   <React.StrictMode>
-    <div>
-      Hello there
+    <div className={styles.app}>
+      <div className={styles['page-wrapper']}>
+        <div className={styles.page}>
+          asd
+        </div>
+      </div>
     </div>
   </React.StrictMode>
 );
